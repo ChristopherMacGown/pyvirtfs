9917c6c5cbbda88f90e03b76888609276672434b
Christopher MacGown <ignoti+github@gmail.com>
2012-02-10 22:48:01 -0800

    Make the setup.py work

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+description = '''A package providing access to the Linux virtual filesystems    
+proc, sysfs, and configfs.'''
+
+setup(name='virtfs',
+      description=description,
+      version='0.9',
+      authors=['Christopher MacGown',],
+      author_email='ignoti@gmail.com',
+      url='',
+      packages=['virtfs', 'virtfs/drivers'],)
